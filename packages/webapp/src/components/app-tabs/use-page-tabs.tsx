1410ebd67352f4226539edf5038b48fe2e309f4d
Edward Knowles <edwardknowles@improbable.io>
2021-02-16 16:40:34 +0000

    fix issue where tabs would not close correctly

--- packages/webapp/src/components/app-tabs/use-page-tabs.tsx
+++ packages/webapp/src/components/app-tabs/use-page-tabs.tsx
@@ -21,13 +21,15 @@ export const usePageTabs = () => {
   if (location.state?.setTab) {
     const { setTab } = location.state;
 
-    // If tab isn't found, add it to the start
-    if (!tabs.some((tab) => tab.id === setTab.id)) {
-      setTabs([{ ...setTab }, ...tabs]);
+    // If tab isn't found, add it to the end
+    if (!tabs.some((tab) => tab.path === setTab.path)) {
+      setTabs([...tabs, { ...setTab }]);
     }
   }
 
   const removeTab = (index: number) => {
+    console.log(index);
+
     // close first and only tab
     if (tabs.length === 1 && index === 0) {
       setTabs([]);
@@ -38,7 +40,7 @@ export const usePageTabs = () => {
     // has multiple tabs
     const newTabs = [...tabs];
     newTabs.splice(index, 1);
-    const nextActiveTab = newTabs[index];
+    const nextActiveTab = newTabs[index - 1] || newTabs[index];
     setTabs(newTabs);
     history.push(nextActiveTab.path, { setTab: nextActiveTab });
   };
