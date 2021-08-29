bc3dbc8aacc62eb1e3cab7bae3326ad4450acd96
Edward Knowles <edwardknowles@improbable.io>
2021-08-29 20:22:54 +0100

    chore(actions): set bundle analyzer mode on ci to json

--- packages/webapp/webpack.prod.js
+++ packages/webapp/webpack.prod.js
@@ -1,11 +1,17 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-  .BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+process.env.NODE_ENV = "production";
 
 module.exports = merge(common, {
   mode: "production",
-  plugins: [new BundleAnalyzerPlugin()],
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: process.env.CI ? "json" : "server",
+    }),
+  ],
   optimization: {
     splitChunks: {
       cacheGroups: {
