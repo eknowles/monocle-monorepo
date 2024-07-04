869acb42a7bc6b666d2d8148d25b1f545301a470
eknowles <817611+eknowles@users.noreply.github.com>
2024-07-04 19:00:25 +0100

    style(components): allow `Side` to scroll vertically

--- packages/components/src/side/side.component.tsx
+++ packages/components/src/side/side.component.tsx
@@ -1,7 +1,7 @@
 import type { FC, PropsWithChildren } from "react";
 
 export const Side: FC<PropsWithChildren<unknown>> = ({ children }) => (
-  <div className="dark:bg-code-800 border-r bg-light-100 dark:border-code-700 w-52 flex h-full items-start flex-col">
+  <div className="dark:bg-code-800 border-r bg-light-100 dark:border-code-700 w-52 flex h-full items-start flex-col overflow-y-auto">
     {children}
   </div>
 );
