168ba53798bdd9b78b406852849d2c21f2926a19
Edward Knowles <817611+eknowles@users.noreply.github.com>
2025-07-10 00:10:33 +0100

    fix: reset initial form values for username and password

--- packages/webapp/src/routes/login/index.tsx
+++ packages/webapp/src/routes/login/index.tsx
@@ -21,7 +21,7 @@ const LoginPage: FC<PropsWithChildren<unknown>> = () => {
   return (
     <div className="w-full h-full flex items-center justify-center">
       <Formik<IFormValues>
-        initialValues={{ username: "admin", password: "" }}
+        initialValues={{ username: "", password: "" }}
         onSubmit={onSubmit}
       >
         <FormLogin title={GRPC_SERVER} />
